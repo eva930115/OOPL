b5ebe41a94978464ceed297ce8729bb38800477a
BryanJhuo <bryan86874928@gmail.com>
2024-03-06 00:38:45 +0800

    add showing coordinate

--- Source/Game/mygame_run.cpp
+++ Source/Game/mygame_run.cpp
@@ -6,6 +6,7 @@
 #include "../Library/gameutil.h"
 #include "../Library/gamecore.h"
 #include "mygame.h"
+#include <sstream>
 
 using namespace game_framework;
 
@@ -33,6 +34,8 @@ void CGameStateRun::OnMove()							// 移動遊戲元素
 void CGameStateRun::OnInit()  								// 遊戲的初值及圖形設定
 {
 	
+	background.LoadBitmapByString({ "Resources/home_page/background.bmp" });
+	background.SetTopLeft(0, 0);
 }
 
 void CGameStateRun::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
@@ -67,4 +70,25 @@ void CGameStateRun::OnRButtonUp(UINT nFlags, CPoint point)	// 處理滑鼠的動作
 
 void CGameStateRun::OnShow()
 {
+	background.ShowBitmap();
+	ShowWindowCoordinate();
+}
+
+void CGameStateRun::ShowWindowCoordinate(){
+	// 座標轉成字串
+	string text;
+	stringstream ss;
+	// 宣告顯示文字工具
+	CDC* pDC = CDDraw::GetBackCDC();
+	// Window 底下的API
+	POINT pt;							// 座標
+	HWND hWnd = GetForegroundWindow();	// 宣告並取得當前視窗的句柄
+	BOOL bReturn = GetCursorPos(&pt);	// 取得當前鼠標在Screen上的座標(若無return false)
+	ScreenToClient(hWnd, &pt);			// 將取得Screen的座標轉成當前視窗句柄的座標
+	if (bReturn != 0) {
+		ss << pt.x << ", " << pt.y;
+		text = ss.str();
+		CTextDraw::Print(pDC, 5, 5, text);
+	}
+	CDDraw::ReleaseBackCDC();
 }
